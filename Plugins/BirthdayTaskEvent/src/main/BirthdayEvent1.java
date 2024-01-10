4fd318afe6f9f5e7b209bf5886967c9c3b6fd17d
Lander <145010136+landeryt@users.noreply.github.com>
2024-01-10 17:13:02 +0300

    Add files via upload

---
+++ Plugins/BirthdayTaskEvent/src/main/BirthdayEvent1.java
@@ -0,0 +1,14 @@
+package me.landeryt.birthdayevent1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BirthdayEvent1 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new Tasks(this), this);
+        getCommand("tokens").setExecutor(new TokensCommand(this));
+    }
+}
